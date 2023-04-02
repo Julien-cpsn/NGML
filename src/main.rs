01885f69e7b8ddd2a58420f230269e0d98fbcde4
Julien cpsn <julien.caposiena@gmail.com>
2023-04-02 17:34:49 +0200

    Code rework with tree approach

--- src/main.rs
+++ src/main.rs
@@ -1,145 +1,84 @@
-use std::fs::File;
-use std::io::{Read, Write};
+use std::fs::{OpenOptions};
+use std::io::Write;
+use std::path::Path;
 use fancy_regex::Regex;
-
+use html5ever::{LocalName};
+use kuchiki::NodeData::{Element};
+use kuchiki::{NodeRef};
+use kuchiki::traits::TendrilSink;
 
 fn main() {
-    const CSS_KEYWORDS: [&str; 2] = ["color", "font-size"];
+    //const CSS_KEYWORDS: [&str; 2] = ["color", "font-size"];
 
     let ngml_path = std::env::args().nth(1).expect("No NGML input file path given");
     let html_path = std::env::args().nth(2).expect("No HTML output file path given");
 
     println!("NGML input :{ngml_path}, HTML output: {html_path}");
 
-    let mut ngml_file = File::open(ngml_path).expect("The given NGML path was not found");
-    let mut html_file = File::create(html_path).expect("The given HTML path was not found");
+    let mut html_file =
+        OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(html_path)
+            .expect("The given HTML path was not found");
 
-    /* Get content */
+    let parser = kuchiki::parse_html();
+    let parsed_file = parser.from_utf8().from_file(Path::new(&ngml_path)).unwrap();
 
-    let mut ngml_content= String::new();
-    ngml_file.read_to_string(&mut ngml_content).expect("Cannot transform file content to string").to_string();
+    let final_tree = manage_node(parsed_file.first_child().unwrap());
 
-    /* Compile */
 
-    /* --- Generic tags --- */
-    // Tags replaced (ordered)
-    let mut replaced_tags = Vec::<String>::new();
+    /* Comments */
+    let mut final_content = final_tree.to_string();
+    let final_content_iterator = final_content.clone();
 
-    // generic tag attributes regex
-    let tag_regex = Regex::new(r#"<(\w+)\s*([^>]*)>"#).unwrap();
-    let tag_attributes_regex = Regex::new(r#"(([\w-]+)=["']?((?:.(?!["']\s+(?:\S+)=|\s*/?[>"']))+.)["']|\w+)"#).unwrap();
-
-    // text tag
-    let h_regex = Regex::new(r"h\d").unwrap();
-
-    let iterator_ngml_content = ngml_content.clone();
-
-    for cap in tag_regex.captures_iter(iterator_ngml_content.as_str())  {
-        let capture = cap.unwrap();
-
-        // Creates the beginning of the new html tag
-        let mut new_tag = String::from("<");
-        let mut new_tag_name = String::new();
-        let mut new_tag_attributes = String::new();
-        let mut tag_attributes_count = 0;
-
-        /* --- ATTRIBUTES --- */
-        // Count tag CSS attributes
-        let mut css_attribute_count = 0;
-        let mut style = String::from("style=\"");
-
-        // Manages attributes
-        for attribute_cap in tag_attributes_regex.captures_iter(&capture[2]) {
-            let attribute_capture = attribute_cap.unwrap();
-
-            if attribute_capture.get(2).is_some() && CSS_KEYWORDS.contains(&&attribute_capture[2]) {
-                style = style + &attribute_capture[2] + ": " + &attribute_capture[3] + ";";
-                css_attribute_count += 1;
-            }
-            else {
-                // Adapts the html tag
-                match &capture[1] {
-                    "text" => {
-                        // Handles h1 .. h6 case
-                        if h_regex.is_match(&attribute_capture[1]).unwrap() {
-                            new_tag_name = attribute_capture[1].to_string();
-                            tag_attributes_count = tag_attributes_count + 1;
-                        }
-                        // Handles reverse text (mirror)
-                        else if attribute_capture[1].eq("reverse") {
-                            new_tag_name = "bdo".to_string();
-                            new_tag_attributes = new_tag_attributes + " dir=\"rtl\"";
-                            tag_attributes_count = tag_attributes_count + 1;
-                        }
-                    }
-                    _ => {
-                        new_tag_name = capture[1].to_string();
-                        tag_attributes_count = tag_attributes_count + 1;
-                    }
-                }
-            }
-        }
-
-        // If no ngml tag had attributes adcapt the new name, attributes and style
-        if tag_attributes_count == 0 {
-            match &capture[1] {
-                "text" => new_tag_name = "p".to_string(),
-                "row" => {
-                    new_tag_name = "div".to_string();
-                    style = style + "display:flex;flex-direction:row;";
-                }
-                _ => new_tag_name = (&capture[1]).to_string()
-            };
-        }
-        // Appends name and attributes to the tag
-        new_tag = new_tag + &new_tag_name + &new_tag_attributes;
-
-        // Append the style to the tag
-        if css_attribute_count > 0 {
-            new_tag = new_tag + " " + &style + "\"";
-        }
-
-        // Finishes the tag
-        new_tag = new_tag + ">"; //+ &capture[3] + "</" + &new_tag_name + ">";
-        // Replaces the ngml tag to html tag
-        ngml_content = ngml_content.replace(&capture[0], new_tag.as_str());
-
-        // Pushes the newly replaces tag to the replaced tag list
-        replaced_tags.push(new_tag_name.clone());
-    }
-
-    // Closing tags
-    let mut closing_tag_counter = 0;
-    let iterator_ngml_content = ngml_content.clone();
-
-    let closing_tag_regex = Regex::new(r#"</(\w+)>"#).unwrap();
-    for cap in closing_tag_regex.captures_iter(iterator_ngml_content.as_str()) {
-        let capture = cap.unwrap();
-        let new_closing_tag = "</".to_owned() + &replaced_tags[closing_tag_counter] + ">";
-
-        ngml_content = ngml_content.replacen(&capture[0], &new_closing_tag, 1);
-
-        closing_tag_counter = closing_tag_counter + 1;
-        // To fix swap problem
-        // <(?'tag'\w+)\s*([^>]*)>(.*</(\k'tag')>)?
-    }
-
-    /* --- Comments --- */
-    // New content iterator
-    let iterator_ngml_content = ngml_content.clone();
-
-    let comment_regex = Regex::new(r#"<\*(?s)(.*?)\*>"#).unwrap();
-    for cap in comment_regex.captures_iter(iterator_ngml_content.as_str())  {
+    let comment_regex = Regex::new(r#"&lt;\*(?s)(.*?)\*&gt;"#).unwrap();
+    for cap in comment_regex.captures_iter(final_content_iterator.as_str())  {
         let capture = cap.unwrap();
 
         let new_comment= "<!--".to_owned() + &capture[1] + "-->";
-        ngml_content = ngml_content.replace(&capture[0], new_comment.as_str());
+        final_content = final_content.replace(&capture[0], new_comment.as_str());
     }
     /* ----- */
 
-    println!();
+    html_file.write_all(final_content.as_bytes()).expect("Cannot write in HTML file");
+}
 
-    /* ----- */
 
-    html_file.write_all(ngml_content.as_bytes()).expect("Cannot write in HTML file");
+fn manage_node(node: NodeRef) -> NodeRef {
+    let new_node: NodeRef;
+    match &node.data() {
+        Element(data) => {
+            let mut new_node_data = data.clone();
+            match new_node_data.name.local.to_string().as_str() {
+                "text" => {
+                    new_node_data.name.local = LocalName::from("p");
+
+                    let h_regex = Regex::new(r"h\d").unwrap();
+                    let attributes = new_node_data.attributes.clone().into_inner().map;
+                    for (name, attribute) in attributes {
+                        println!("{:?} {:?}", name.local.to_string(), attribute.value);
+
+                        if h_regex.is_match(name.local.to_string().as_str()).unwrap() {
+                            new_node_data.name.local = LocalName::from(name.local.to_string());
+                            new_node_data.attributes.get_mut().map.remove(&name);
+                        }
+                    }
+                    println!()
+                }
+                _ => {}
+            }
+            new_node = NodeRef::new(Element(new_node_data));
+        }
+        _ => {
+            new_node = node.clone()
+        }
+    }
+
+
+    for child in node.children() {
+        new_node.append(manage_node(child));
+    }
+
+    return new_node;
 }
